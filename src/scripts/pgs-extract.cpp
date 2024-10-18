39595e0ef650c248eb2cdaafabf2d198ed0a43ed
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2024-10-18 15:59:36 +0300

    Convert test.cpp to a script

--- src/scripts/pgs-extract.cpp
+++ src/scripts/pgs-extract.cpp
@@ -1,26 +1,34 @@
+#include <CLI/CLI.hpp>
 #include <boost/gil.hpp>
 #include <boost/gil/extension/io/png.hpp>
 #include <boost/gil/image_view.hpp>
 #include <boost/gil/locator.hpp>
 #include <boost/gil/step_iterator.hpp>
+#include <filesystem>
 #include <fstream>
 #include <pgs.hpp>
 
-template<typename T>
-void print_bytes(T&& range) {
-	for (auto val : range) std::cerr << std::format("{:#x} ", std::bit_cast<uint8_t>(val));
-	std::cerr << "\n";
-}
-
-void main_() {
+int main_(int argc, char** argv) {
 	using namespace std;
 	using namespace pgs;
-
 	namespace bg = boost::gil;
+	namespace fs = std::filesystem;
+
+	CLI::App app;
+	argv = app.ensure_utf8(argv);
+
+	std::string path;
+	app.add_option("file", path, ".pub source file");
+
+	try {
+		app.parse(argc, argv);
+	} catch (const CLI::ParseError& e) {
+		return app.exit(e);
+	}
 
 	ifstream ifs;
 	ifs.exceptions(ios::badbit | ios::failbit);
-	ifs.open("test.sup", ios::binary);
+	ifs.open(path, ios::binary);
 
 	auto segments = serial::loaditer<Segment>(ifs);
 	auto subs = subtitles(segments);
@@ -33,18 +41,26 @@ void main_() {
 			reinterpret_cast<bg::rgba8_pixel_t*>(sub.bitmap.data()),
 			sub.bitmap.width() * 4
 		);
-		bg::write_view(std::format("out-{}.png", sub.timestamp.time_since_epoch().count()), bitmap_view, bg::png_tag{});
+
+		auto timestamp = sub.timestamp.time_since_epoch().count();
+		auto duration = sub.duration.count();
+
+		bg::write_view(
+			std::format("{}.{}-{}.png", fs::path{path}.stem().string(), timestamp, duration), bitmap_view, bg::png_tag{}
+		);
 	}
+
+	return 0;
 }
 
-int main() {
+int main(int argc, char** argv) {
 #ifdef NDEBUG
 	try {
-		main_();
+		return main_(argc, argv);
 	} catch (std::runtime_error& e) {
 		std::cout << e.what() << "\n";
 	}
 #else
-	main_();
+	return main_(argc, argv);
 #endif
 }
